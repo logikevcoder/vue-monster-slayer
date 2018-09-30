a6327acce66fab1fb76c03895351cf7e116f39de
“Kevin <logikevcoder@gmail.com>
2018-09-30 13:39:01 -0500

    added attack button functionality

--- app.js
+++ app.js
@@ -10,6 +10,45 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    attack: function() {
+      this.monsterHealth -= this.calculateDamage(3, 10);
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.playerHealth -= this.calculateDamage(6, 12);
+      this.checkWin();
+    },
+    special: function() {
+
+    },
+    heal: function() {
+
+    },
+    giveUp: function() {
+
+    },
+    calculateDamage: function(min, max) {
+      return damage = Math.max(Math.floor(Math.random() * max) + 1, min); // generate random num between 3 - 10
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if(confirm('You won! New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if(confirm('You won! New Game?')) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
     }
   }
 })
